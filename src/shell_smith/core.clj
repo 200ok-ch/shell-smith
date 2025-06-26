9051fbdb997b0652691c6ba73f2d66478286580e
Phil Hofmann <phil@200ok.ch>
2025-06-26 21:23:13 +0200

    .

--- src/shell_smith/core.clj
+++ src/shell_smith/core.clj
@@ -1,9 +1,5 @@
 ; -*- mode: clojure -*-
 
-;; NOTE: this now lives in bb.edn
-;; (require '[babashka.deps :as deps])
-;; (deps/add-deps '{:deps {dev.nubank/docopt {:mvn/version "0.6.1-fix7"}}})
-
 (ns shell-smith.core
   (:require [clojure.walk :as walk]
             [clj-yaml.core :as yaml]
@@ -11,11 +7,11 @@
             [babashka.fs :as fs]
             [docopt.core :as docopt]))
 
-(defn- config-from-file [path]
+(defn config-from-file [path]
   (when (fs/exists? path)
     (-> path slurp yaml/parse-string)))
 
-(defn- config-from-env [pattern]
+(defn config-from-env [pattern]
   (let [env (System/getenv)
         kees (filter (partial re-find pattern) (keys env))
         t (fn [[k v]] [(-> k (str/replace pattern "") str/lower-case keyword) v])]
@@ -34,7 +30,7 @@
   (let [t (fn [[k v]] [(simplify-key k) v])]
     (into {} (map t opts))))
 
-(defn- config-from-cli [usage args]
+(defn config-from-cli [usage args]
   (docopt/docopt usage
                  args
                  (comp drop-boring
@@ -42,14 +38,18 @@
                        simplify-keys)))
 
 (defmacro config [usage# & {:as opts#}]
-  `(let [config-defaults# (get ~opts# :defaults)
-         cli-name#        (get ~opts# :name ~(str *ns*))
-         config-file#     (str (fs/cwd) "/" cli-name# ".yml")
-         env-var-prefix#  (str/upper-case cli-name#)
-         env-var-regexp#  (re-pattern (str "^" env-var-prefix# "_"))
-         args#            (get ~opts# :args *command-line-args*)]
+  `(let [config-defaults#       (get ~opts# :defaults)
+         cli-name#              (get ~opts# :name ~(str *ns*))
+         config-file-config#    (str (System/getenv "XDG_CONFIG_HOME") "/" cli-name# "/config.yml")
+         config-file-oldschool# (str (System/getenv "HOME") "/." cli-name# ".yml")
+         config-file-local#     (str (fs/cwd) "/" cli-name# ".yml")
+         env-var-prefix#        (str/upper-case cli-name#)
+         env-var-regexp#        (re-pattern (str "^" env-var-prefix# "_"))
+         args#                  (get ~opts# :args *command-line-args*)]
      (merge config-defaults#
-            (config-from-file config-file#)
+            (config-from-file config-file-config#)
+            (config-from-file config-file-oldschool#)
+            (config-from-file config-file-local#)
             (config-from-env env-var-regexp#)
             (config-from-cli ~usage# args#))))
 
